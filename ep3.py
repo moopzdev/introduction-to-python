ff68b8983725aa769fba3f2ab994310a9abc3e27
Moopz-Dev <pongzthor@gmail.com>
2022-03-31 23:22:30 +0700

    add: ep3 print()

---
+++ ep3.py
@@ -0,0 +1,16 @@
+# String
+print("String")
+
+# Number (integer,float)
+print(-1)
+print(0)
+print(1)
+print(3.99)
+
+# Boolean
+print(True)
+print(False)
+
+# escapes
+print("New Line \n" + "New Line")
+print("Tab \t" + "Tab")
