0db147e559b73afeab698135dbadd2daa37a2c4e
Moopz-Dev <pongzthor@gmail.com>
2022-03-31 23:53:10 +0700

    add: ep5 naming _variAble$

---
+++ ep5.py
@@ -0,0 +1,14 @@
+#naming variables
+
+'''
+1. allows alphanumerics and symbols
+2. disallows beginning with number and symbols exceptfor "_"
+3. disallows naming with restricted keywords
+4. variables are case-sensitive
+'''
+
+a=10
+a1="Moopz-Dev"
+_a22e="Begins with lodash"
+_A22E="case sensitive"
+print(a1)
