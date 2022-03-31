151e6a01b3a7525741002698029b39398a019246
Moopz-Dev <pongzthor@gmail.com>
2022-03-31 23:44:28 +0700

    add: ep4 type(str(3.99))

---
+++ ep4.py
@@ -0,0 +1,9 @@
+# Data Types & Variables
+
+x = 20
+y = 3.99
+z = True
+print("result = " + str(x)) # using str() method to convert number to string
+print(y)
+print(z)
+print(type(z)) # logs type of argument to the console
