264f2d6afa95c0e8a052c09962073920851bb5b0
Moopz-Dev <pongzthor@gmail.com>
2022-04-01 09:57:08 +0700

    add: ep8.py "operators and operands"

---
+++ ep8.py
@@ -0,0 +1,11 @@
+#operators and operands
+x = 10
+y = 4
+
+print("summation: ",x+y)
+print("subtraction",x-y)
+print("multiplication",x*y)
+print("division",x/y)
+print("floored division",x//y)
+print("power",x**y)
+print("modulo",x%y)
