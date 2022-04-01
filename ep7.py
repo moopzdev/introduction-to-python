a76320a8892e67c8076306d8b4991e50fe6f1c4e
Moopz-Dev <pongzthor@gmail.com>
2022-04-01 09:47:12 +0700

    add: ep7 "input"

---
+++ ep7.py
@@ -0,0 +1,10 @@
+#input
+
+# fName = input("Enter your first name:") # save user input as 'fName' variable
+# lName = input("Enter your last name:")
+# print("You entered : " + fName + ' and ' + lName)
+# print(type(fName)) # str
+
+x = input("Enter x: ")
+y = input("Enter y: ")
+print("the sum of two number is: " + str(int(x)+int(y)))
