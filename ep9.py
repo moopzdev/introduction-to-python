4abe5b814b3de094cf29678268be40f98ca74604
Moopz-Dev <pongzthor@gmail.com>
2022-04-01 12:16:34 +0700

    add: ep9.py  "comparators"

---
+++ ep9.py
@@ -0,0 +1,11 @@
+# comparing values
+
+x, y, z = 100, 15, 30
+
+print("x > y ? : ", x > y)
+print("x < y ? : ", x < y)
+print("x equals y ? : ", x == y)
+print("x does not equal y ? : ", x != y)
+print("x is greater than or equals y ? : ", x >= y)
+print("x is less than or equals y ? : ", x <= y)
+print("x is less than y which is less than z ? :", x < y < z)
