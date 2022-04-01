406a43842fa5ade1552b6c9a9706ee083f3fad31
Moopz-Dev <pongzthor@gmail.com>
2022-04-01 13:30:48 +0700

    add: ep11 "compound assignments"

---
+++ ep11.py
@@ -0,0 +1,18 @@
+# compound assignments
+
+x = 10
+print("@line4: ", x)
+x += 5  # x = x+5
+print("@line6: ", x)
+x -= 10
+print("@line8: ", x)
+x *= 10
+print("@line10: ", x)
+x **= 2
+print("@line12: ", x)
+x /= 2
+print("@line14: ", x)
+x //= 2
+print("@line16: ", x)
+x %= 3
+print("@line18: ", x)
